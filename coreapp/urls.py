f460aa539610d4727c77d26d6b0fe2249d42a19e
FX2One <dfoxik@gmail.com>
2022-01-20 14:11:02 +0100

    FIX:get_absolute_url around the views, UPDATE: AddView model

--- coreapp/urls.py
+++ coreapp/urls.py
@@ -5,5 +5,7 @@ app_name = 'coreapp'
 
 urlpatterns = [
     path('',views.IndexView.as_view(), name='index'),
-    path('<slug:slug>/', views.SingleView.as_view(), name='single')
+    path('add',views.AddView.as_view(),name='add'),
+    path('comics/', views.PostView.as_view(),name='comics'),
+    path('<slug:slug>/', views.SingleView.as_view(), name='single'),
 ]
--- coreapp/views.py
+++ coreapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Core
-from django.views.generic import ListView, DetailView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView, CreateView
+from django.urls import reverse_lazy
 # Create your views here.
 
 class IndexView(ListView):
@@ -11,3 +12,15 @@ class SingleView(DetailView):
     model = Core
     template_name = 'coreapp/single.html'
     context_object_name = 'comicbook'
+
+class PostView(ListView):
+    model = Core
+    template_name = 'coreapp/comics.html'
+    context_object_name = 'comics_list'
+
+class AddView(CreateView):
+    model = Core
+    fields = '__all__' #taking all fields from DB
+    template_name = 'coreapp/add.html'
+    context_object_name = 'add'
+    success_url = reverse_lazy('coreapp:comics') #taking users back to comics crud view
