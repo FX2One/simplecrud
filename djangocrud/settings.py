c962102383db4f7f7d964a454b9d4e44a057c798
FX2One <dfoxik@gmail.com>
2022-01-20 14:32:06 +0100

    building up forms with widget_tweaks

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'coreapp'
+    'coreapp',
+    'widget_tweaks'
 ]
 
 MIDDLEWARE = [
